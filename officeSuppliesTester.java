4ab3f2acd49edbc59229d4efe0536ec7dda94637
Juliet Lord <jlord24@students.polytechnic.org>
2023-01-30 14:55:40 -0800

    wrote tester for tally counter

--- officeSuppliesTester.java
+++ officeSuppliesTester.java
@@ -1,3 +1,34 @@
 public class officeSuppliesTester {
-    
+    public static void main(String[] args) {
+        //test tally counter  
+        int testsPassed = 0; 
+        System.out.println("Creating a new tally counter...");
+        tallyCounter a = new tallyCounter();
+        testsPassed++;
+        System.out.println("Clicking the tally counter...");
+        a.click();
+        a.click();
+        a.click();
+        a.click();
+        a.click();
+        testsPassed++;
+        System.out.println("Checking the count...");
+        System.out.println(a.getClicks());
+        testsPassed++;
+        System.out.println("Checking if the count is correct...");
+        if(a.getClicks() == 5){
+            System.out.println("Counter is correct");
+            testsPassed++;
+        } else {
+            System.out.println("Failed");
+        }
+        System.out.println("Results: " + testsPassed + ("/4"));
+
+        
+
+
+        //test folder
+
+        //test snack cabinet
+    }
 }
