70ade49cd31bcd3b8249297eb97a63be4c97474b
Kate Lim <klim24@students.polytechnic.org>
2023-01-30 14:44:28 -0800

    outlined methods

--- snackCabinet.java
+++ snackCabinet.java
@@ -1,3 +1,27 @@
 public class snackCabinet {
-    
+    //fields
+    private String chips;
+    private String candy;
+    private String drinks;
+
+    //constructors
+    public snackCabinet(String myChips, String myCandy, String myDrinks){
+        chips = myChips;
+        candy = myCandy;
+        drinks = myDrinks;
+    }
+
+    //methods
+
+    public String getChips;
+
+    public String getCandy;
+
+    public String getDrinks;
+
+    public void openCabinet;
+
+    public void closeCabinet;
+
+    public void randomSnack;
 }
