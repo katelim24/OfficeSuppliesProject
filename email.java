f3eb10eb904cef8adfac6138489ed601b474622b
Kate Lim <klim24@students.polytechnic.org>
2023-01-31 22:00:28 -0800

    edited email method

---
+++ email.java
@@ -0,0 +1,16 @@
+public class email {
+    //fields
+    private String username;
+    private String password;
+    private String draft;
+
+    //constructors
+    public email(String myUsername, String myPassword, String myDraft){
+        username = myUsername;
+        password = myPassword;
+        draft = myDraft;
+    }
+
+    //methods
+
+}
