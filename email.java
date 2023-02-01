4405eadbbd0d8df7a218e5afb46f4ec6dc6649b3
Kate Lim <klim24@students.polytechnic.org>
2023-01-31 22:07:23 -0800

    updated methods

--- email.java
+++ email.java
@@ -3,14 +3,26 @@ public class email {
     private String username;
     private String password;
     private String draft;
+    private String inbox;
 
     //constructors
-    public email(String myUsername, String myPassword, String myDraft){
-        username = myUsername;
-        password = myPassword;
+    public email(String myUsername, String myPassword, String myDraft, String myInbox){
+        username = "panther";
+        password = "apcompsci23";
         draft = myDraft;
+        inbox = myInbox;
     }
 
     //methods
+    public boolean checkUsername(){
 
+    }
+
+    public boolean checkPassword(){
+
+    }
+
+    public void recieveEmail(){
+        
+    }
 }
