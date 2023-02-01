1867f639d6d5d6a9f36cbe2aaff520e8b977c178
Juliet Lord <jlord24@students.polytechnic.org>
2023-01-31 22:24:48 -0800

    password variable

--- email.java
+++ email.java
@@ -24,11 +24,13 @@ public class email {
 
     public boolean checkPassword(String myPassword){
         for(int i = 0; i < password.length(); i++){
+            int password = 1;
             int count = 0;
             int curr = i;
             int next = i + 1;
-            if(curr == 1){
+            if(curr == password){
                 count++;
+                password++;
                 return "digit correct, enter next number";
             } else{
                 return "wrong digit,retry";
